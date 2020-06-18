b6144b3f049b6fdc77715a09a6283fd5f4647ea2
vatsraghu <55733143+vatsraghu@users.noreply.github.com>
2020-06-18 07:23:42 +0530

    Update 3_odd_even_numbers.py
    you just appended even numbers in the list!
    i just corrected that minor error

--- DataStructures/2_Arrays/Solution/3_odd_even_numbers.py
+++ DataStructures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -3,7 +3,7 @@ max = int(input("Enter max number: "))
 odd_numbers = []
 
 for i in range(max):
-    if i%2 == 1:
+    if i%2 != 1:
         odd_numbers.append(i)
 
 print("Odd numbers: ",odd_numbers)
